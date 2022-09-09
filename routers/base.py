0581e1dc46164b5991e90c14d44e475601342e4e
alanmgg <alanfmorag@gmail.com>
2022-09-08 23:57:46 -0500

    :arrow_up: Initial commit

---
+++ routers/base.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, APIRouter
+from fastapi.middleware.cors import CORSMiddleware
+
+router = APIRouter()
+
+app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@router.get("/", tags=["Base"])
+async def read_root():
+    return {"Hello": "World"}
+
+
+@router.get("/items/{item_id}", tags=["Base"])
+async def read_item(item_id: int):
+    return {"item_id": item_id}
