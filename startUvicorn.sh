67624758e1002447d8f6d904df4512ac4505f4b1
alanmgg <alanfmorag@gmail.com>
2022-09-09 21:53:24 -0500

    :up: Uvicorn service created

--- startUvicorn.sh
+++ startUvicorn.sh
@@ -2,7 +2,8 @@
 
 UVI_BIN_DIR="/home/ubuntu/.local/bin"
 UVI_PATH="/home/ubuntu/FastAPI-BD"
+LOG_PATH="/home/ubuntu/logs"
 
 echo "Starting uvicorn process ..."
 
-python3 -m $UVI_BIN_DIR/uvicorn --app-dir $UVI_PATH main:app --reload --reload-dir $UVI_PATH/ --host '0.0.0.0' --port 8018 > $UVI_PATH/logs/uvicorn.log 2>&1
+python3 -m $UVI_BIN_DIR/uvicorn --app-dir $UVI_PATH main:app --reload --reload-dir $UVI_PATH/ --host '0.0.0.0' --port 8018 > $LOG_PATH/uvicorn.log 2>&1
