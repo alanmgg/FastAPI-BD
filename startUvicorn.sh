74d527631dd8eaa5c391b474457274d3034377b0
alanmgg <alanfmorag@gmail.com>
2022-09-09 22:32:25 -0500

    :up: Uvicorn service created

--- startUvicorn.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-LOG_PATH="/home/ubuntu/logs"
-
-echo "Starting uvicorn process ..."
-
-python3 -m uvicorn main:app --reload --host '127.0.0.1' --port 8018 > $LOG_PATH/uvicorn.log 2>&1
