a05d61b238cd1f94c202ba692d2edd82ba674249
alanmgg <alanfmorag@gmail.com>
2022-09-28 15:35:52 -0500

    :new: Environment variables used

--- apiServer/routers/db/database.py
+++ apiServer/routers/db/database.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://admin:MalopeFI3#DB@db-oracle.cln5k8wdcnlt.us-east-1.rds.amazonaws.com:3306/bookmysql'
+import env
+
+SQLALCHEMY_DATABASE_URL = env.MYSQL_CONNECTION
 
 engine = None
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
--- env/__init__.py
+++ env/__init__.py
@@ -1,3 +1,5 @@
 # Configuraciones globales
 APP_DIR = "/opt/app"
 APP_DIR_WINDOWS = "/"
+
+MYSQL_CONNECTION = "mysql+pymysql://admin:MalopeFI3#DB@db-oracle.cln5k8wdcnlt.us-east-1.rds.amazonaws.com:3306/bookmysql"
