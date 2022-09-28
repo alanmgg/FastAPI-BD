227c63107e696f91f2b0d2855ea6c73ba679ac2f
alanmgg <alanfmorag@gmail.com>
2022-09-28 15:51:08 -0500

    :new: Uvicorn services were created

---
+++ startUvicorn.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+UVI_BIN_DIR="/home/ubuntu/.local/bin"
+UVI_PATH="/home/ubuntu/FastAPI-BD"
+UVI_LOGS="/home/ubuntu/logs"
+
+echo "Starting uvicorn process ..."
+
+$UVI_BIN_DIR/uvicorn --app-dir $UVI_PATH apiServer:app --reload --reload-dir $UVI_PATH/ --host '0.0.0.0' --port 8018 > $UVI_LOGS/uvicorn.log 2>&1
