5303c733b2564e47fca0990b31f3cdaf6d905855
alanmgg <alanfmorag@gmail.com>
2022-09-28 20:01:11 -0500

    :new: Uvicorn services were created

--- initUvicorn.sh
+++ initUvicorn.sh
@@ -5,6 +5,6 @@ UVI_PATH="/home/ubuntu"
 echo "Starting init uvicorn process ..."
 
 sudo systemctl stop uvicorn_app.service
-sudo rm -rf $UVI_PATH/FastAPI-BD
-git clone https://github.com/alanmgg/FastAPI-BD.git $UVI_PATH
+sudo rm -rf $UVI_PATH/FastAPI-BD/
+git clone https://github.com/alanmgg/FastAPI-BD.git $UVI_PATH/
 sudo systemctl start uvicorn_app.service
