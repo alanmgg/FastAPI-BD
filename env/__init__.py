8a04b67d178b08b08206245d3a55d7d2912667e0
alanmgg <alanfmorag@gmail.com>
2022-10-04 18:23:31 -0500

    :recycle: Image paths fixed

--- env/__init__.py
+++ env/__init__.py
@@ -4,5 +4,5 @@ APP_DIR_WINDOWS = "/"
 
 MYSQL_CONNECTION = "mysql+pymysql://admin:MalopeFI3#DB@db-oracle.cln5k8wdcnlt.us-east-1.rds.amazonaws.com:3306/bookmysql"
 
-IMAGE_DIR = "images/"
+IMAGE_DIR = "/home/ubuntu/FastAPI-BD/images/"
 IMAGE_DIR_WINDOWS = "/images/"
